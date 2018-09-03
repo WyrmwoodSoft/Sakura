c7ec98c3a39a71dae2ed5c9b0eb76332c1342553
matt <metropolitan.matt@gmail.com>
2018-09-03 00:56:16 -0400

    Adding some real content - images, text, jquery, javascript, css, and some midwares.

---
+++ Sakura/wwwroot/js/Index.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    console.log("Welcome to Wyrmwood Gaming!");
+
+    var button = document.getElementById("testButton");
+    button.addEventListener("click", function () {
+        console.log("test click");
+    });
+
+    var navigation = $(".navbarInfo li");
+    navigation.on("click", function () {
+        console.log("Clicked: " + $(this).text());
+    });
+});
